d9944b19f731b3f930d71252d7620f63644f8a89
John Mateer <jj.mateer@live.com>
2019-08-25 14:47:50 -0700

    Could not find a way to update the list when more characters are put in with the previous functions in letter.js. Working on making function in word.js to push letter objects to an array and use the check function in letter.js to change the guessed property to true or false.

--- letter.js
+++ letter.js
@@ -10,42 +10,43 @@ const Letter = function (input, character, guessed, check, placeholder, answerLo
 Letter.prototype.placeholder = "_"
 
 var checkArray = [];
-Letter.prototype.checkArr = checkArray;
+var checkArray2 = [];
 Letter.prototype.check = function () {
     if (this.input === this.character) {
         // console.log(this.character)
         // console.log('Correct')
         this.guessed = true;
-        this.charStay = true;
         // console.log("guessed: " + this.guessed)
 
-    } else {
+    }else if (this.input !== this.character) {
         // console.log(this.character)
         // console.log("Incorrect")
         this.guessed = false;
-        this.charStay = false;
+
         // console.log("guessed: " + this.guessed)
 
     }
-    Letter.prototype.returnLetter();
+    // Letter.prototype.returnLetter();
 }
 
-Letter.prototype.returnLetter = function () {
-    if (this.guessed === true) {
-        checkArray.push(this.character)
-    }
-    if (this.guessed === false) {
-        checkArray.push(this.placeholder)
-    }
-}
-Letter.prototype.display = function () {
-    console.log(checkArray)
-    console.log('answerlog: ' + this.answerLogLetter)
-    
-    emptycheck()
+// Letter.prototype.returnLetter = function () {
+//     if (this.guessed === true) {
+//         checkArray.push(this.character)
+//     }
+//     if (this.guessed === false) {
+//         checkArray.push(this.placeholder)
+//     }
 
-}
-function emptycheck() {
-    checkArray = [];
-}
+// }
+// Letter.prototype.display = function () {
+//     // console.log(checkArray)
+//     // console.log(checkArray2)
+//     console.log('answerlog: ' + this.answerLogLetter)
+
+//     // emptycheck()
+
+// }
+// function emptycheck() {
+//     checkArray = [];
+// }
 module.exports = Letter
--- word.js
+++ word.js
@@ -10,25 +10,25 @@ const Word = function (convertWord, splitWord, words, loopChars, input, answerLo
 var currentWord = [];
 var words = ['television', 'computer', 'automobile']
 var letterArray = []
-
+var letArr2 = [];
 Word.prototype.randomWord = function () {
-        var random = Math.floor(Math.random() * 3) + 1;
-        if (random === 1) {
-            currentWord.push(words[0])
-            // console.log(currentWord)
-        }
-        if (random === 2) {
-            currentWord.push(words[1])
-            // console.log(currentWord)
-        }
-        if (random === 3) {
-            currentWord.push(words[2])
-            // console.log(currentWord)
-        }
-        if(currentWord.length > 1) {
-            currentWord.shift();
-        }
+    var random = Math.floor(Math.random() * 3) + 1;
+    if (random === 1) {
+        currentWord.push(words[0])
+        // console.log(currentWord)
     }
+    if (random === 2) {
+        currentWord.push(words[1])
+        // console.log(currentWord)
+    }
+    if (random === 3) {
+        currentWord.push(words[2])
+        // console.log(currentWord)
+    }
+    if (currentWord.length > 1) {
+        currentWord.shift();
+    }
+}
 
 Word.prototype.splitWord = function () {
     for (var i = 0; i < currentWord.length; i++) {
@@ -43,25 +43,44 @@ Word.prototype.convertWord = function () {
             var letterObj = new Letter(this.input, currentWord[i][j], false)
             letterArray.push(letterObj)
             // console.log(letterObj)
+            // console.log(currentWord[i][j])
+            // console.log(letterArray)
         }
-        // console.log("Letter array: " + (JSON.stringify(letterArray)))
     }
-    Word.prototype.loopChars()
-}
-Word.prototype.loopChars = function () {
+    // Word.prototype.loopChars = function () {
     for (var i = 0; i < letterArray.length; i++) {
         Letter.prototype.input = letterArray[i].input
         Letter.prototype.character = letterArray[i].character
         Letter.prototype.answerLogLetter = this.answerLogWord
-        // console.log(this.answerLogWord)
+        // console.log(letterArray[i])
         // console.log("Letter " + JSON.stringify(letterArray[i].input))
         // console.log("char " + JSON.stringify(letterArray[i].character))
         Letter.prototype.check()
+        var guessed = Letter.prototype.guessed
+        console.log(guessed)
     }
-    console.log(letterArray)
-    Letter.prototype.display()
+    returnLetters();
     emptyLetter();
-    
+    // Word.prototype.loopChars()
+}
+// Word.prototype.loopChars = function () {
+//     for (var i = 0; i < letterArray.length; i++) {
+//         Letter.prototype.input = letterArray[i].input
+//         Letter.prototype.character = letterArray[i].character
+//         Letter.prototype.answerLogLetter = this.answerLogWord
+//         console.log(letterArray[i].character)
+//         // console.log("Letter " + JSON.stringify(letterArray[i].input))
+//         // console.log("char " + JSON.stringify(letterArray[i].character))
+//         Letter.prototype.check()
+//     }
+// returnLetters();
+// // console.log(letterArray)
+// // Letter.prototype.display()
+// emptyLetter();
+
+// }
+function returnLetters() {
+    console.log(letArr2)
 }
 function emptyLetter() {
     letterArray = [];
